25b96087e82827df07f496be973b1ebb8b057c3c
hasanalali2003 <hasanalali887h@gmail.com>
2024-09-27 07:45:34 +0100

    Task 2

--- router/general.js
+++ router/general.js
@@ -17,8 +17,8 @@ public_users.get("/", function (req, res) {
 // Get book details based on ISBN
 public_users.get("/isbn/:isbn", function (req, res) {
     const isbn = req.params.isbn;
-    console.log(isbn);
-    return res.status(300).json({ message: "Yet to be implemented" });
+    const book = Object.values(books).find((book) => book.isbn === isbn);
+    return res.status(300).json(JSON.stringify(book));
 });
 
 // Get book details based on author
