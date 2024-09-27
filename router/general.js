90e2c32d7e6186a0ad558ae2a0f2a0511352f7b5
hasanalali2003 <hasanalali887h@gmail.com>
2024-09-27 07:35:27 +0100

    Add isbn for testing

--- router/general.js
+++ router/general.js
@@ -16,7 +16,8 @@ public_users.get("/", function (req, res) {
 
 // Get book details based on ISBN
 public_users.get("/isbn/:isbn", function (req, res) {
-    //Write your code here
+    const isbn = req.params.isbn;
+    console.log(isbn);
     return res.status(300).json({ message: "Yet to be implemented" });
 });
 
