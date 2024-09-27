31b28320fc0b5be092ce005db27fdfcd9eff54a8
hasanalali2003 <hasanalali887h@gmail.com>
2024-09-27 07:18:23 +0100

    Commit initial files

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const session = require("express-session");
+const customer_routes = require("./router/auth_users").authenticated;
+const genl_routes = require("./router/general").general;
+
+const app = express();
+
+app.use(express.json());
+
+app.use(
+    "/customer",
+    session({
+        secret: "fingerprint_customer",
+        resave: true,
+        saveUninitialized: true,
+    })
+);
+
+app.use("/customer/auth", function auth(req, res, next) {
+    if (req.session.authorization) {
+        let token = req.session.authorization["accessToken"];
+
+        jwt.verify(token, "access", (err, user) => {
+            if (!err) {
+                req.user = user;
+                next();
+            } else {
+                return res
+                    .status(403)
+                    .json({ message: "User not authencatied" });
+            }
+        });
+    }
+});
+
+const port = 3000;
+
+app.use("/customer", customer_routes);
+app.use("/", genl_routes);
+
+app.listen(port, () => {
+    console.log("Listening to port : ", port);
+});
